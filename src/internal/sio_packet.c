297d42555bef9090809eaa110621d0c4ddcdc6dd
Dominik Völkel <voelkel.dominik@gmail.com>
2023-10-14 12:49:10 +0200

    fix base64 binary received data

--- src/internal/sio_packet.c
+++ src/internal/sio_packet.c
@@ -3,10 +3,90 @@
 #include <utility.h>
 
 #include <esp_log.h>
+
 #include <sio_client.h>
 
 const char *TAG = "[sio_packet]";
 const char *empty_str = "";
+
+static const char base64_table[65] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+// https://github.com/espressif/esp-idf/blob/8fc8f3f47997aadba21facabc66004c1d22de181/components/wpa_supplicant/src/utils/base64.c#L84C1-L150C2
+static unsigned char *base64_gen_decode(const char *src, size_t len,
+                                        size_t *out_len)
+
+{
+    const char *table = base64_table;
+    unsigned char dtable[256], *out, *pos, block[4], tmp;
+    size_t i, count, olen;
+    int pad = 0;
+    size_t extra_pad;
+
+    memset(dtable, 0x80, 256);
+    for (i = 0; i < sizeof(base64_table) - 1; i++)
+        dtable[(unsigned char)table[i]] = (unsigned char)i;
+    dtable['='] = 0;
+
+    count = 0;
+    for (i = 0; i < len; i++)
+    {
+        if (dtable[(unsigned char)src[i]] != 0x80)
+            count++;
+    }
+
+    if (count == 0)
+        return NULL;
+    extra_pad = (4 - count % 4) % 4;
+
+    olen = (count + extra_pad) / 4 * 3;
+    pos = out = malloc(olen);
+    if (out == NULL)
+        return NULL;
+
+    count = 0;
+    for (i = 0; i < len + extra_pad; i++)
+    {
+        unsigned char val;
+
+        if (i >= len)
+            val = '=';
+        else
+            val = src[i];
+        tmp = dtable[val];
+        if (tmp == 0x80)
+            continue;
+
+        if (val == '=')
+            pad++;
+        block[count] = tmp;
+        count++;
+        if (count == 4)
+        {
+            *pos++ = (block[0] << 2) | (block[1] >> 4);
+            *pos++ = (block[1] << 4) | (block[2] >> 2);
+            *pos++ = (block[2] << 6) | block[3];
+            count = 0;
+            if (pad)
+            {
+                if (pad == 1)
+                    pos--;
+                else if (pad == 2)
+                    pos -= 2;
+                else
+                {
+                    /* Invalid padding */
+                    free(out);
+                    return NULL;
+                }
+                break;
+            }
+        }
+    }
+
+    *out_len = pos - out;
+    return out;
+}
 
 void parse_packet(Packet_t *packet)
 {
@@ -28,6 +108,27 @@ void parse_packet(Packet_t *packet)
         packet->eio_type = EIO_PACKET_OK_SERVER;
         packet->sio_type = SIO_PACKET_NONE;
         packet->json_start = NULL;
+        return;
+    }
+
+    if (packet->data[0] == 'b')
+    {
+        packet->eio_type = EIO_PACKET_MESSAGE;
+        packet->sio_type = SIO_PACKET_BINARY_EVENT;
+
+        // Careful with this! binary data starts offset by 1 and is base64 encdoded
+
+        char *decoded_b64 = (char *)base64_gen_decode(packet->data + 1, packet->len - 1, &packet->len);
+
+        if (decoded_b64 == NULL)
+        {
+            ESP_LOGE(TAG, "Failed to decode base64 dataset from SIO");
+            return;
+        }
+        free(packet->data);
+        packet->data = decoded_b64;
+        packet->json_start = NULL;
+
         return;
     }
 
